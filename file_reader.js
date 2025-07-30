50e91ab84f458221071326c05868b02fa3a90992
dueon <hdejh79@gmail.com>
2025-07-29 20:38:04 -0700

    I'm done today's assignment

--- file_reader.js
+++ file_reader.js
@@ -2,4 +2,14 @@ const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
   // your code here
-})
+
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      let hobbies = JSON.parse(fs.readFileSync("./hobbies.txt"));
+
+      console.log(
+        `${data1} ${data2} is ${data3} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`
+      );
+    });
+  });
+});
